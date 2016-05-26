f4687f09a40d4754fee8dacfa2a765c1cb688849
James Wilkins <james.wilkins@fasthosts.com>
2016-05-26 13:35:16 +0100

    Updating with fixes to deployment

--- Dockerfile
+++ Dockerfile
@@ -1,33 +1,29 @@
 FROM 1and1internet/ubuntu-16-apache-2.4-php-5.6:unstable
+ARG DEBIAN_FRONTEND=noninteractive
+
+COPY files/ /
 
 WORKDIR /var/www/html
 
-# install the PHP extensions we need
-RUN apt-get update && apt-get install -y libpng12-dev libjpeg-dev php5.6-gd php5.6-mysql curl && rm -rf /var/lib/apt/lists/* 
+# Environment variables for the MySQL DB
+ENV WORDPRESS_DB_HOST=mysql \
+    WORDPRESS_DB_USER=username \
+    WORDPRESS_DB_NAME=databasename \
+    WORDPRESS_DB_PASSWORD=EnvVarHere \
+    WORDPRESS_DB_PREFIX='wp_'
 
-RUN a2enmod rewrite expires
-
-# set recommended PHP.ini settings
-# see https://secure.php.net/manual/en/opcache.installation.php
-RUN { \
+RUN \
+apt-get update &&\
+apt-get install -y unzip libpng12-dev libjpeg-dev php5.6-gd php5.6-mysql curl &&\
+rm -rf /var/lib/apt/lists/* &&\
+a2enmod rewrite expires &&\
+{ \
     echo 'opcache.memory_consumption=128'; \
     echo 'opcache.interned_strings_buffer=8'; \
     echo 'opcache.max_accelerated_files=4000'; \
     echo 'opcache.revalidate_freq=60'; \
     echo 'opcache.fast_shutdown=1'; \
     echo 'opcache.enable_cli=1'; \
-  } > /etc/php/5.6/apache2/conf.d/10-opcache.ini
-
-# upstream tarballs include ./wordpress/ so this gives us /usr/src/wordpress
-RUN curl -o wordpress.tar.gz -SL https://wordpress.org/latest.tar.gz \
-  && tar -xzf wordpress.tar.gz -C /usr/src/ \
-  && rm wordpress.tar.gz \
-  && chown -R www-data:www-data /usr/src/wordpress
-
-# Wordpress DB_Host defaults to 3306. Define as host:port if not on 3306
-ENV WORDPRESS_DB_HOST=mysql \
-    WORDPRESS_DB_USER=wordpress \
-    WORDPRESS_DB_NAME=wordpress \
-    WORDPRESS_DB_PASSWORD=EnvVarHere 
-
-COPY supervisord-pre /hooks/supervisord-pre
+  } > /etc/php/5.6/apache2/conf.d/10-opcache.ini && \
+curl -o /usr/src/wordpress.tar.gz -fSL https://wordpress.org/latest.tar.gz &&\
+chmod -R 755 /hooks /init
---
+++ spec/spec_helper.rb
@@ -0,0 +1,67 @@
+require 'serverspec'
+require 'docker'
+
+MYSQL_ROOT_PASSWORD="super-secret-password"
+MYSQL_USER="fhwordpresstest"
+MYSQL_PASSWORD="super-secret-password1"
+MYSQL_DATABASE="fhwordpresstest"
+MYSQL_HOST="mysqltesting"
+
+#Include Tests
+base_spec_dir = Pathname.new(File.join(File.dirname(__FILE__)))
+Dir[base_spec_dir.join('../../drone-tests/shared/**/*.rb')].sort.each { |f| require_relative f }
+
+if not ENV['IMAGE'] then
+  puts "You must provide an IMAGE env variable"
+end
+@network = Docker::Network.create "wordpresstest"
+@mysqlimage = Docker::Image.create('fromImage' => 'mysql:latest')
+@container = Docker::Container.create(
+  'name'           => MYSQL_HOST,
+  'Image'          => @mysqlimage.id,
+  'Config'         => { "Hostname" => "#{MYSQL_HOST}"},
+  'HostConfig'     => {
+     'NetworkMode' => @network.info["Name"],
+  },
+  'Env'            => [
+    "MYSQL_ROOT_PASSWORD=#{MYSQL_ROOT_PASSWORD}",
+    "MYSQL_USER=#{MYSQL_USER}",
+    "MYSQL_PASSWORD=#{MYSQL_PASSWORD}",
+    "MYSQL_DATABASE=#{MYSQL_DATABASE}"
+  ]
+)
+@container.start
+
+
+LISTEN_PORT=8080
+CONTAINER_START_DELAY=10
+
+RSpec.configure do |c|
+  @image = Docker::Image.get(ENV['IMAGE'])
+  set :backend, :docker
+  set :docker_image, @image.id
+  set :docker_container_create_options, {
+      'User'           => '100000',
+      'Host Config'    => {
+         'NetworkMode' => @network.info["Name"],
+      },
+      'Env'            => [
+        "WORDPRESS_DB_USER=#{MYSQL_USER}",
+        "WORDPRESS_DB_PASSWORD=#{MYSQL_PASSWORD}",
+        "WORDPRESS_DB_NAME=#{MYSQL_DATABASE}",
+        "WORDPRESS_DB_HOST=#{MYSQL_HOST}"
+  ]
+
+  }
+
+  describe "tests" do
+    include_examples 'docker-ubuntu-16'
+    include_examples 'docker-ubuntu-16-apache-2.4'
+    include_examples 'php-5.6-tests'
+    include_examples 'wordpress'
+  end
+end
+
+@container.kill
+@container.delete
+@network.delete
