2cd5fe179567e5523ee9222c534ca7dfcdb36a33
James Poole <james.poole@fasthosts.com>
2016-07-11 13:16:22 +0100

    Updated spec_helper.rb to new standard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,7 @@
+require 'rspec'
 require 'serverspec'
 require 'docker'
-
-MYSQL_ROOT_PASSWORD="super-secret-password"
-MYSQL_USER="fhwordpresstest"
-MYSQL_PASSWORD="super-secret-password1"
-MYSQL_DATABASE="fhwordpresstest"
-MYSQL_HOST="mysqltesting"
+require_relative '../../drone-tests/shared/jemonkeypatch.rb'
 
 #Include Tests
 base_spec_dir = Pathname.new(File.join(File.dirname(__FILE__)))
@@ -15,49 +11,21 @@ if not ENV['IMAGE'] then
   puts "You must provide an IMAGE env variable"
 end
 
-#@network = Docker::Network.create "wordpresstest1"
-#@mysqlimage = Docker::Image.create('fromImage' => 'mysql:latest')
-#@container = Docker::Container.create(
-#  'name'           => MYSQL_HOST,
-#  'Image'          => @mysqlimage.id,
-#  'Config'         => { "Hostname" => "#{MYSQL_HOST}"},
-#  'HostConfig'     => {
-#     'NetworkMode' => @network.info["Name"],
-#  },
-#  'Env'            => [
-#    "MYSQL_ROOT_PASSWORD=#{MYSQL_ROOT_PASSWORD}",
-#    "MYSQL_USER=#{MYSQL_USER}",
-#    "MYSQL_PASSWORD=#{MYSQL_PASSWORD}",
-#    "MYSQL_DATABASE=#{MYSQL_DATABASE}"
-#  ]
-#)
-#@container.start
-
-
 LISTEN_PORT=8080
-CONTAINER_START_DELAY=10
+
+set :backend, :docker
+@image = Docker::Image.get(ENV['IMAGE'])
+set :docker_image, @image.id
+#set :docker_debug, true
+set :docker_container_start_timeout, 60
+set :docker_container_ready_regex, /READY/
+
+set :docker_container_create_options, {
+  'Image'      => @image.id,
+  'User'       => '100000',
+}
 
 RSpec.configure do |c|
-  @image = Docker::Image.get(ENV['IMAGE'])
-  set :backend, :docker
-  set :docker_image, @image.id
-  set :docker_container_create_options, {
-      'User'           => '100000',
-      #'Host Config'    => {
-      #   'NetworkMode' => @network.info["Name"],
-      #},
-      'Env'            => [
-        "WORDPRESS_DB_USER=#{MYSQL_USER}",
-        "WORDPRESS_DB_PASSWORD=#{MYSQL_PASSWORD}",
-        "WORDPRESS_DB_NAME=#{MYSQL_DATABASE}",
-        "WORDPRESS_DB_HOST=#{MYSQL_HOST}"
-  ]
-
-  }
-
-  describe command("sleep #{CONTAINER_START_DELAY}") do
-    its(:stdout) { should eq "" }
-  end
 
   describe "tests" do
     include_examples 'docker-ubuntu-16'
@@ -66,7 +34,3 @@ RSpec.configure do |c|
     include_examples 'wordpress'
   end
 end
-
-#@container.kill
-#@container.delete
-#@network.delete
